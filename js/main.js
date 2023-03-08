d0c23bf93b65f015f5c82c46454b6e1b6be4fb48
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-03-08 15:14:54 +0100

    array visibile

--- js/main.js
+++ js/main.js
@@ -1,37 +1,59 @@
+//MILESTONE 0
 //creo un array di oggetti:
-const team =[
+const team = [
     {
         'nome_cognome': 'Wayne Barnett',
-        'ruolo' : 'Founder & CEO',
-        'foto':'wayne-barnett-founder-ceo.jpg'
+        'ruolo': 'Founder & CEO',
+        'foto': 'wayne-barnett-founder-ceo.jpg'
     },
     {
         'nome_cognome': 'Angela Caroll ',
-        'ruolo' : 'Chief Editor',
-        'foto':'angela-caroll-chief-editor.jpg'
-    },   
+        'ruolo': 'Chief Editor',
+        'foto': 'angela-caroll-chief-editor.jpg'
+    },
     {
         'nome_cognome': 'Walter Gordon ',
-        'ruolo' : 'Office Manager',
-        'foto':'walter-gordon-office-manager.jpg'
+        'ruolo': 'Office Manager',
+        'foto': 'walter-gordon-office-manager.jpg'
     },
     {
         'nome_cognome': 'Angela Lopez',
-        'ruolo' : 'Social Media Manager',
-        'foto':'angela-lopez-social-media-manager.jpg'
+        'ruolo': 'Social Media Manager',
+        'foto': 'angela-lopez-social-media-manager.jpg'
     },
     {
         'nome_cognome': 'Scott Estrada',
-        'ruolo' : 'Develope',
-        'foto':'scott-estrada-developer.jpg'
+        'ruolo': 'Develope',
+        'foto': 'scott-estrada-developer.jpg'
     },
     {
         'nome_cognome': 'Barbara Ramos',
-        'ruolo' : 'Graphic Designer',
-        'foto':'barbara-ramos-graphic-designer.jpg'
+        'ruolo': 'Graphic Designer',
+        'foto': 'barbara-ramos-graphic-designer.jpg'
     }
 ]
 
 // MILESTONE 1
+//Array visibile in console
 console.log(team);
+
+
+const cardDom = document.getElementById('card');
+for (let i = 0; i < team.length; i++) {
+    let member = team[i];
+
+    for(let key in member){
+        console.log(key + '-'+ member[key]);
+        cardDom.innerHTML += `<div class="member-card"> 
+                                <span>${key} ---> </span> 
+                                <span>${member[key]}</span>
+                             </div>`
+    }
+}
+
+
+
+
+
+
 
