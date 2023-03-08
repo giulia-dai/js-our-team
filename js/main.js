9d807f2e3bf3061eab28cc46fe50c596b568331e
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-03-08 13:21:26 +0100

    array di oggetti

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//creo un array di oggetti:
+const team =[
+    {
+        'nome_cognome': 'Wayne Barnett',
+        'ruolo' : 'Founder & CEO',
+        'foto':'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nome_cognome': 'Angela Caroll ',
+        'ruolo' : 'Chief Editor',
+        'foto':'angela-caroll-chief-editor.jpg'
+    },   
+    {
+        'nome_cognome': 'Walter Gordon ',
+        'ruolo' : 'Office Manager',
+        'foto':'walter-gordon-office-manager.jpg'
+    },
+    {
+        'nome_cognome': 'Angela Lopez',
+        'ruolo' : 'Social Media Manager',
+        'foto':'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nome_cognome': 'Scott Estrada',
+        'ruolo' : 'Develope',
+        'foto':'scott-estrada-developer.jpg'
+    },
+    {
+        'nome_cognome': 'Barbara Ramos',
+        'ruolo' : 'Graphic Designer',
+        'foto':'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+// MILESTONE 1
+console.log(team);
+
