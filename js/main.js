015bb49ae09de2c1c3ec4971a219ae6976d248dc
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-03-09 10:16:47 +0100

    immagini visibili

--- js/main.js
+++ js/main.js
@@ -2,54 +2,54 @@
 //creo un array di oggetti:
 const team = [
     {
-        'nome_cognome': 'Wayne Barnett',
+        'nome': 'Wayne Barnett',
         'ruolo': 'Founder & CEO',
         'foto': 'wayne-barnett-founder-ceo.jpg'
     },
     {
-        'nome_cognome': 'Angela Caroll ',
+        'nome': 'Angela Caroll ',
         'ruolo': 'Chief Editor',
         'foto': 'angela-caroll-chief-editor.jpg'
     },
     {
-        'nome_cognome': 'Walter Gordon ',
+        'nome': 'Walter Gordon ',
         'ruolo': 'Office Manager',
         'foto': 'walter-gordon-office-manager.jpg'
     },
     {
-        'nome_cognome': 'Angela Lopez',
+        'nome': 'Angela Lopez',
         'ruolo': 'Social Media Manager',
         'foto': 'angela-lopez-social-media-manager.jpg'
     },
     {
-        'nome_cognome': 'Scott Estrada',
+        'nome': 'Scott Estrada',
         'ruolo': 'Develope',
         'foto': 'scott-estrada-developer.jpg'
     },
     {
-        'nome_cognome': 'Barbara Ramos',
+        'nome': 'Barbara Ramos',
         'ruolo': 'Graphic Designer',
         'foto': 'barbara-ramos-graphic-designer.jpg'
     }
 ]
 
-// MILESTONE 1
-//Array visibile in console
-console.log(team);
-
-
-//MILESTONE 2
+//MILESTONE 1 E 2
 //Rendere l'array visibile nella pagina
+
 const cardDom = document.getElementById('card');
 for (let i = 0; i < team.length; i++) {
     let member = team[i];
+    console.log`(${member.name}, ${member.ruolo}, ${member.foto})`;
 
     for(let key in member){
-        console.log(key + '-'+ member[key]);
-        cardDom.innerHTML += `<div class="member-card"> 
-                                <span>${key} ---> </span> 
-                                <span>${member[key]}</span>
-                             </div>`
+        console.log(key + ' '+ member[key]);
+        cardDom.innerHTML +=
+                            `<div class="member-card"> 
+                                <h2>${member.nome}</h2> 
+                                <h4>${member.ruolo}</h4>
+                                <img src="img/${member.foto}"  alt="${member.name}"/>
+                             </div>
+                             `;
     }
 }
 
